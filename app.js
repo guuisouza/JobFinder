9441104192f648a23c1ea3d2ac9f843c9c007242
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-09-02 16:00:05 -0300

    feat: Implementando routes de jobs e bodyParser

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
 const db = require('./db/connection')
+const bodyParser = require('body-parser')
 
 const PORT = 3000
 
 app.listen(PORT, () => {
     console.log(`O Express está rodando na porta ${PORT}`)
 })
+
+// boddy parser no express
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // db connection
 db
@@ -22,3 +26,6 @@ db
 app.get('/', (req, res) => {
     res.send("Funcionando 3")
 })
+
+// jobs routes
+app.use('/jobs', require('./routes/jobs'))
