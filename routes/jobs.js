92a747bc7cab2849a084b456400fbb4cd8d2b140
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-09-03 12:57:18 -0300

    feat: Implementando consulta de detalhe das vagas

--- routes/jobs.js
+++ routes/jobs.js
@@ -8,6 +8,18 @@ router.get('/test', (req,res)=>{
     res.send("Deu certo")
 })
 
+// detalhe da vaga
+router.get('/view/:id', (req, res) => {
+    Job.findOne({
+        where: {id: req.params.id}
+    }).then(job => {
+        res.render('view', {
+            job
+        })
+    }).catch(err => console.log(err))
+})
+
+// form da rota de envio
 router.get('/add', (req, res) => {
     res.render('add')
 })
